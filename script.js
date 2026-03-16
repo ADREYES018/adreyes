9441aec30b96c2f62bd7e7274e041e9e9e5e3795
ADREYES018 <justinadrielr@gmail.com>
2026-03-16 15:40:56 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const site = {
   logoText: "Adriel Reyes",
   logoUrl: "https://adrielreyes.my.canva.site/adshome/",
-  menu: ["Experiences", "About Me", "Contact Me"],
+  menu: "[
+  { label: "Experinces", url: "https://adrielreyes.my.canva.site/adshome/experiences" },
+  { label: "About Me", url: "https://adrielreyes.my.canva.site/adshome/about-me" },
+  { label: "Contact Me", url: "https://adrielreyes.my.canva.site/adshome/contact" }
+]",
   sideLabel: "Categories +",
   heroWord: "my works",
   endingTitle: "you finished this category.",
